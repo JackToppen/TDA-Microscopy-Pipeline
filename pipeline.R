a000f961081b288ee31fcd0eae252a6b0d4771a2
JackToppen <jtoppen3@gatech.edu>
2022-04-10 17:53:30 -0400

    Updated import statement for functions.R in pipeline file.

--- pipeline.R
+++ pipeline.R
@@ -1,51 +1,36 @@
 
-# ------------------------- Normalize GFP/RFP Values --------------------------
-# setwd("~/Documents/Research/TDA/R/")
-# 
-# # iterate over experimental conditions
-# for (signal in c("Nanog_Gata6")) {
-#   for (conc in c(0, 5, 15, 25)) {
-#     for (well in c(1, 2, 3)) {
-#       for (loc in c(1, 2, 3, 4, 5)) {
-#         # get path to CSV file
-#         old_path <- paste0("data/", signal, "/concentration_", conc, "/")
-#         
-#         # make new directory for storing normalized GFP/RFP values
-#         new_path <- paste0("normalized/", signal, "/concentration_", conc, "/")
-#         dir.create(new_path, recursive=TRUE, showWarnings=FALSE)
-#         
-#         # get CSV filename
-#         file_name <- paste0(signal, "_", conc, "_", well, "_", loc, ".csv")
-#         
-#         # read the CSV, normalize the GFP/RFP, and save to new CSV
-#         df <- read.csv(paste0(old_path, file_name))
-#         df <- normalize(df)
-#         save_to_csv(df, paste0(new_path, file_name))
-#       }
-#     }
-#   }
-# }
+------------------------- Normalize GFP/RFP Values --------------------------
+setwd("~/Documents/Research/TDA/R/")
+
+# iterate over experimental conditions
+for (signal in c("Nanog_Gata6")) {
+  for (conc in c(0, 5, 15, 25)) {
+    for (well in c(1, 2, 3)) {
+      for (loc in c(1, 2, 3, 4, 5)) {
+        # get path to CSV file
+        old_path <- paste0("data/", signal, "/concentration_", conc, "/")
+
+        # make new directory for storing normalized GFP/RFP values
+        new_path <- paste0("normalized/", signal, "/concentration_", conc, "/")
+        dir.create(new_path, recursive=TRUE, showWarnings=FALSE)
+
+        # get CSV filename
+        file_name <- paste0(signal, "_", conc, "_", well, "_", loc, ".csv")
+
+        # read the CSV, normalize the GFP/RFP, and save to new CSV
+        df <- read.csv(paste0(old_path, file_name))
+        df <- normalize(df)
+        save_to_csv(df, paste0(new_path, file_name))
+      }
+    }
+  }
+}
 
 
 # --------------------- Reconstruct Microscopy Images -------------------------
+setwd("~/Documents/Research/TDA/R/TDA-Microscopy-Pipeline/")
+source("functions.R")
 setwd("~/Documents/Research/TDA/R/")
-
-
-gen_table <- function(df, gfp_thresh, rfp_thresh) {
-  # count the number of cells and get the GFP/RFP data frame columns
-  total_cells = nrow(df)
-  GFP <- df[, 6]
-  RFP <- df[, 7]
-  
-  # get the distribution of cell types
-  M00 = sum(((RFP < rfp_thresh) + (GFP < gfp_thresh)) == 2) / total_cells
-  M11 = sum(((RFP > rfp_thresh) + (GFP > gfp_thresh)) == 2) / total_cells
-  M01 = sum(((RFP < rfp_thresh) + (GFP > gfp_thresh)) == 2) / total_cells
-  M10 = sum(((RFP > rfp_thresh) + (GFP < gfp_thresh)) == 2) / total_cells
-  
-  # store values in a 2x2 matrix
-  return(matrix(c(M10, M00, M11, M01), ncol=2))
-}
 
 # dox concentrations
 concentrations <- c(0, 5, 15, 25)
@@ -93,8 +78,8 @@ for (sig in c("Nanog_Gata6")) {
       perc <- percentiles[[perc_index]]
       
       # generate thresholds based percentile
-      gfp_threshold <- gen_threshold(percentile, gfp_values)
-      rfp_threshold <- gen_threshold(percentile, rfp_values)
+      gfp_threshold <- gen_threshold(perc, gfp_values)
+      rfp_threshold <- gen_threshold(perc, rfp_values)
       
       # iterate over doxycycline treatment concentrations
       for (dox_index in c(1, 2, 3, 4)) {
