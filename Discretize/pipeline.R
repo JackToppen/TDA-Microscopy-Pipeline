448771165d5ea0038ae731f8eaa0277f54560ed5
JackToppen <jtoppen3@gatech.edu>
2022-04-19 08:06:50 -0400

    Reorganized pipeline to add Discretize and TDA sections

---
+++ Discretize/pipeline.R
@@ -0,0 +1,117 @@
+
+# ------------------------- Normalize GFP/RFP Values --------------------------
+
+# set working directory to location of micrscopy CSVs
+setwd("~/Documents/Research/TDA/R/")
+
+# iterate over treatment groups
+for (sig in c("Nanog_Gata6", "Nanog_HA")) {
+  for (conc in c(0, 5, 15, 25)) {
+    for (well in c(1, 2, 3)) {
+      for (loc in c(1, 2, 3, 4, 5)) {
+        # get path to microscopy CSV file
+        csv_path <- paste0("data/", sig, "/concentration_", conc, "/")
+
+        # make new directory for storing normalized GFP/RFP values
+        norm_path <- paste0("normalized/", signal, "/concentration_", conc, "/")
+        dir.create(norm_path, recursive=TRUE, showWarnings=FALSE)
+
+        # get the new CSV filename and full path
+        file_name <- paste0(sig, "_", conc, "_", well, "_", loc, ".csv")
+
+        # read the CSV, normalize the GFP/RFP, and save to new CSV
+        df <- read.csv(paste0(csv_path, file_name))
+        df <- normalize(df)
+        save_to_csv(df, paste0(norm_path, file_name))
+      }
+    }
+  }
+}
+
+
+# --------------------- Reconstruct Microscopy Images -------------------------
+
+# pull functions from functions.R scripts
+setwd("~/Documents/Research/TDA/R/TDA-Microscopy-Pipeline/")
+source("functions.R")
+
+# set working directory to location of micrscopy CSVs
+setwd("~/Documents/Research/TDA/R/")
+
+# hold actual values for dox concentrations and threshold percentiles
+concentrations <- c(0, 5, 15, 25)
+percentiles <- c(75, 80, 85, 90, 95)
+
+# store distributions of cell types in a 2D list of 2x2 matrices
+M <- list()
+for (perc_index in c(1, 2, 3, 4, 5)) {
+  M[[perc_index]] <- list()
+  for (dox_index in c(1, 2, 3, 4)) {
+    M[[perc_index]][[dox_index]] <- matrix(rep(0,4), ncol=2)
+  }
+}
+
+# iterate over treatment groups
+for (sig in c("Nanog_Gata6", "Nanog_HA")) {
+  for (loc in c(1, 2, 3, 4, 5)) {
+    # store location specific expression values for generating thresholds
+    gfp_values <- list()
+    rfp_values <- list()
+    
+    # load baseline CSVs for generating thresholds
+    for (well in c(1, 2, 3)) {
+      # directory path for baseline expression values
+      dir_gfp <- paste0("normalized/", sig, "/concentration_25/")
+      dir_rfp <- paste0("normalized/", sig, "/concentration_0/")
+      
+      # get CSV file names
+      gfp_file <- paste0(sig, "_25_", well, "_", loc, ".csv")
+      rfp_file <- paste0(sig, "_0_", well, "_", loc, ".csv")
+      
+      # read the CSV to data frames
+      gfp_df <- read.csv(paste0(dir_gfp, gfp_file))
+      rfp_df <- read.csv(paste0(dir_rfp, rfp_file))
+      
+      # hold location specific expression values for GFP/RFP
+      gfp_values[[well]] <- gfp_df[, 6]
+      rfp_values[[well]] <- rfp_df[, 7]
+    }
+    
+    for (perc_index in c(1, 2, 3, 4, 5)) {
+      # get the percentile value
+      perc <- percentiles[[perc_index]]
+      
+      # generate thresholds based percentile
+      gfp_threshold <- gen_threshold(perc, gfp_values)
+      rfp_threshold <- gen_threshold(perc, rfp_values)
+      
+      for (dox_index in c(1, 2, 3, 4)) {
+        # get dox concentration value
+        conc <- concentrations[[dox_index]]
+          
+        for (well in c(1, 2, 3)) {
+          # get path to CSV file and file name
+          csv_path <- paste0("normalized/", sig, "/concentration_", conc, "/")
+          csv_name <- paste0(sig, "_", conc, "_", well, "_", loc, ".csv")
+          
+          # read the CSV, discretize the GFP/RFP values, and assign colors
+          df <- read.csv(paste0(csv_path, csv_name))
+          df <- discretize(df, gfp_threshold, rfp_threshold)
+          df <- assign_color(df, show_double_low=TRUE)
+          
+          # get/make directory for storing images
+          image_path <- paste0("images/", perc, "/", sig, "/concentration_", conc, "/")
+          dir.create(image_path, recursive=TRUE, showWarnings=FALSE)
+          
+          # get image name and save
+          image_name <- paste0(sig, "_", conc, "_", well, "_", loc, ".png")
+          save_image(df, paste0(image_path, image_name))
+          
+          # add to distribution table
+          value <- gen_table(df, gfp_threshold, rfp_threshold)
+          M[[perc_index]][[dox_index]] <- M[[perc_index]][[dox_index]] + value
+        }
+      }
+    }
+  }
+}
